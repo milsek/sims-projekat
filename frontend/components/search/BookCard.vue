4647fd692ac94cb109ef1a6f1810fec0a5b0a739
Milan Sekulic <sekulic.sw54.2019@uns.ac.rs>
2021-06-12 21:16:57 +0200

    Added reservation buttons

--- frontend/components/search/BookCard.vue
+++ frontend/components/search/BookCard.vue
@@ -3,17 +3,26 @@
     <div class="flex pb-10 mx-auto">
       <div class="inline-block w-40 sm:w-36 md:w-32">
           <img :src="require(`@/assets/images/${data.image}`)"  alt="" class="rounded-lg object-cover w-full">
+          <div class="block sm:hidden pt-2">
+            <button class="h-8 md:h-7 lg:h-8 px-6 sm:px-4 lg:px-6 pb-1 bg-gradient-to-tr from-red-400 to-purple-400 text-white
+            text-center text-lg md:text-base lg:text-lg shadow-md focus:outline-none hover:text-gray-900 rounded-lg">reservation</button>
+          </div>
       </div>
-      <div class="minline-block font-display pl-4 md:pl-6 w-5/6">
-        <div class="text-base md:text-xl font-bold">
-          <NuxtLink :to="{ name: '/', params: {id: data.id}}" class="hover:text-blue-800 hover:opacity-80 w-auto">
+      <div class="inline-block font-display pl-4 md:pl-6 w-5/6">
+        <div class="flex justify-between text-base md:text-xl font-bold">
+          <NuxtLink :to="{ name: 'index', params: {id: data.id}}" class="hover:text-blue-800 hover:opacity-80 w-auto">
               {{ data.title }}
           </NuxtLink>
+        <div class="hidden sm:block -mt-1">
+          <button class="h-8 md:h-7 lg:h-8 px-6 sm:px-4 lg:px-6 pb-1 bg-gradient-to-tr from-red-400 to-purple-400 text-white
+          text-center text-lg md:text-base lg:text-lg shadow-md focus:outline-none rounded-lg
+          hover:transition duration-300 ease-in-out transform hover:-translate-y-1">reservation</button>
+        </div>
         </div>
         <div class="text-sm font-light pb-2 text-gray-500">
           {{ data.author }}
         </div>
-        <div class="text-xs md:text-sm text-justify font-light md:tracking-wide mb-4 line-clamp-3 md:line-clamp-2 xl:line-clamp-3 text-gray-400">
+        <div class="text-sm text-justify font-light md:tracking-wide mb-4 line-clamp-4 md:line-clamp-2 xl:line-clamp-3 text-gray-400">
           {{ data.description }}
         </div>
         <div class="pb-0 inline-block font-thin align-middle text-sm text-gray-400">Reads: {{readsInThousands}}k</div>
--- frontend/components/search/SearchLayout.vue
+++ frontend/components/search/SearchLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block md:flex w-11/12 mx-auto rounded-lg bg-white">
+  <div class="block md:flex w-11/12 mx-auto rounded-lg bg-white shadow-lg">
     <div class="p-4 md:p-8 h-full">
       <BookFilter />
     </div>
