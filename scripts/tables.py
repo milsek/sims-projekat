010c8a1b828144fe29a7d303150a752545257426
s4ndu <lazicdusan1104@gmail.com>
2021-06-16 05:36:03 +0200

    Added support for relationship between EDITION and TAG, CONTENT

--- scripts/tables.py
+++ scripts/tables.py
@@ -10,15 +10,14 @@ class EDITION:
 
         self.id = len(EDITION.instances)
         self.available_copies = random.randint(5, 60)
-        self.title = info.get('title').replace("'","''")
+        self.title = info.get('title').replace("'", "''")
 
         author_name = info.get('authors')[0]
         publisher_name = info.get('publisher')
 
-        contributor_names = info.get('authors')[1:]
-        tag_names = ['tagovi', 'idu', 'ovde']
-        content_names = info.get('categories')
-
+        self.contributor_names = info.get('authors')[1:]
+        self.tag_names = ['tagovi', 'idu', 'ovde']
+        self.content_names = info.get('categories')
 
         self.description = info.get('description')
         self.dimensions = random.randint(18, 22)
@@ -34,29 +33,39 @@ class EDITION:
 
         if len(requests.get(self.image_large).content) in [9103, 316]:
             return
+        if self.description:
+            self.description = self.description.replace("'", "''")
+        else:
+            return
+        if not self.content_names:
+            return
         if not self.rating:
             self.rating = random.randint(10, 50) / 10
         if self.year:
             self.year = info.get('publishedDate')[:4]
         else:
             self.year = '2021'
-        if self.description:
-            self.description = self.description.replace("'", "''")
-        else:
-            return
 
         self.author_id = PERSON.author_with_name(author_name).id
         self.publisher_id = PUBLISHER.with_name(publisher_name).id
-        # TODO: Book type class, publisher id, content, contributors, tags
 
     def toSql(self):
-        sql = f"INSERT INTO BOOK_TYPE VALUES ({self.id}, {self.available_copies}, '{self.title}');\n" \
-              f"INSERT INTO EDITION VALUES ('{self.description}', '{self.dimensions}', '{self.image_large}', '{self.image_small}', " \ 
-              f"'{self.language}', {self.page_count}, {self.rating}, {self.reads}, {self.take_out}, PARSEDATETIME('{self.year}', 'yyyy'), " \
-              f"{self.id}, {self.author_id}, {self.publisher_id});\n"
+        sql = [f"INSERT INTO BOOK_TYPE VALUES ({self.id}, {self.available_copies}, '{self.title}');",
+               f"INSERT INTO EDITION VALUES ('{self.description}', '{self.dimensions}', '{self.image_large}', '{self.image_small}', "
+               f"'{self.language}', {self.page_count}, {self.rating}, {self.reads}, {self.take_out}, PARSEDATETIME('{self.year}', 'yyyy'), "
+               f"{self.id}, {self.author_id}, {self.publisher_id});"]
 
+        for contrib_name in self.contributor_names:
+            contrib = PERSON.contributor_with_name(contrib_name)
+            sql.append(f"INSERT INTO EDITION_CONTRIBUTORS VALUES({self.id}, {contrib.id});")
+        for tag_name in self.tag_names:
+            tag = TAG.with_name(tag_name)
+            sql.append(f"INSERT INTO EDITION_TAGS VALUES({self.id}, {tag.id});")
+        for content_name in self.content_names:
+            content_id = CONTENT.with_name(content_name).id
+            sql.append(f"INSERT INTO EDITION_CONTENT VALUES({self.id}, {content_id});")
 
-        return
+        return sql
 
 
 class PERSON:
@@ -112,3 +121,41 @@ class PUBLISHER:
             return PUBLISHER.map.get(name)
         else:
             return PUBLISHER(name)
+
+
+class TAG:
+    instances = []
+    map = {}
+
+    def __init__(self, name):
+        self.id = len(TAG.instances)
+        self.name = name
+
+        TAG.instances.append(self)
+        TAG.map[name] = self
+
+    @staticmethod
+    def with_name(name):
+        if TAG.map.get(name):
+            return TAG.map.get(name)
+        else:
+            return TAG(name)
+
+
+class CONTENT:
+    instances = []
+    map = {}
+
+    def __init__(self, name):
+        self.id = len(CONTENT.instances)
+        self.name = name
+
+        CONTENT.instances.append(self)
+        CONTENT.map[name] = self
+
+    @staticmethod
+    def with_name(name):
+        if CONTENT.map.get(name):
+            return CONTENT.map.get(name)
+        else:
+            return CONTENT(name)
