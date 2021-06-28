70fb6c37d1e84a3f294a4047b5bb73cd1b922fe8
Pavle <pavleg.19@gmail.com>
2021-06-29 00:50:51 +0200

    Added cookies:)

--- backend/main/java/com/example/library/controller/AccountController.java
+++ backend/main/java/com/example/library/controller/AccountController.java
@@ -6,6 +6,9 @@ import com.example.library.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
@@ -16,8 +19,16 @@ public class AccountController {
 
     @GetMapping(path = "/login/")
     @ResponseBody
-    public User getEdition(@RequestParam(name = "mail") String mail,
-                           @RequestParam(name = "password") String password) {
-        return accountService.login(mail, password);
+    public User login(@RequestParam(name = "mail") String mail,
+                           @RequestParam(name = "password") String password,
+                           HttpServletResponse response) {
+        User user = accountService.login(mail, password);
+        if(user != null) {
+            Cookie nameCookie = new Cookie("mail", mail);
+            response.addCookie(nameCookie);
+            Cookie roleCookie = new Cookie("role", String.valueOf(user.getDiscriminatorValue()));
+            response.addCookie(roleCookie);
+        }
+        return user;
     }
 }
