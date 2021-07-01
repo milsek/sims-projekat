78d6921ed591a376af59c916d7859e57e95eec79
Pavle <pavleg.19@gmail.com>
2021-07-02 01:48:09 +0200

    Minor selected book vue change

--- frontend/components/admin/exchange/SelectedBook.vue
+++ frontend/components/admin/exchange/SelectedBook.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="block md:flex mx-auto rounded-xl border border-dashed border-gray-300 p-6">
     <div class="flex-shrink-0">
-<<<<<<< HEAD
-      <img :src="data.edition.imageLarge" alt=""
-      class="text-center sm:text-left rounded-md  w-36 lg:w-40 h-56
-      lg:h-60 object-fill mx-auto" draggable="false">
-=======
       <img :src="data.editionImageLarge" alt=""
       class="text-center sm:text-left rounded-md
       w-36 lg:w-40 h-56 lg:h-60 object-fill mx-auto" draggable="false">
->>>>>>> 435dd0c58eef3e5701d71e32f0295feac77aefa6
     </div>
     <div class="md:pl-4 text-center md:text-left">
       <div class="text-xl md:text-base lg:text-xl font-bold text-blue-900 opacity-90
