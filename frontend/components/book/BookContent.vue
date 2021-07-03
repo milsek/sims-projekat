9d1f99ecc8e5e24ba085df5a889cc0c744f4f6f6
Milan Sekulic <sekulic.sw54.2019@uns.ac.rs>
2021-07-03 23:50:01 +0200

    Refactored BookContent some more

--- frontend/components/book/BookContent.vue
+++ frontend/components/book/BookContent.vue
@@ -81,25 +81,10 @@
 
       </div>
     </div>
-
-    <div class="block w-11/12 mt-6 mx-auto bg-white shadow-lg">
-      <div class="block p-8 w-full">
-        <div class="text-xl text-gray-700 tracking-wide font-medium">Details</div>
-          <div class="grid grid-rows-flow sm:grid-rows-5 lg:grid-rows-4 sm:grid-flow-col sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 text-sm font-medium text-gray-700 pt-2">
-            <div v-for="att in attributes" :key="att.name" class="truncate">
-              <div class="pt-2">{{att.text}}: <span class="font-normal text-gray-600 truncate"> {{getData(att)}}</span></div>
-            </div>
-          </div>
-      </div>
-    </div>
-
-    <div class="block w-11/12 mt-6 mx-auto bg-white shadow-lg">
-      <div class="block p-8 w-full">
-        <div class="text-xl text-gray-700 tracking-wide font-medium">Related editions</div>
-        <RelatedEditions />
-      </div>
-    </div>
     
+    <Details :data="data" />
+    <RelatedEditions />
+
     <LeaveReview v-if="userCanReview" @send-review="sendReview" />
     <ReviewConfirmationModal v-if="showReviewConfirmation" @close-modal="closeReviewConfirmationModal" />
 
@@ -110,6 +95,7 @@
 </template>
 
 <script>
+import Details from '~/components/book/Details'
 import RelatedEditions from '~/components/book/RelatedEditions'
 import ReviewList from '~/components/book/ReviewList'
 import LeaveReview from '~/components/book/LeaveReview'
@@ -117,19 +103,10 @@ import ReviewConfirmationModal from '~/components/book/ReviewConfirmationModal.v
 import axios from 'axios'
 export default {
   props: ["data"],
-  components: { RelatedEditions, ReviewList, LeaveReview, ReviewConfirmationModal },
+  components: { Details, RelatedEditions, ReviewList, LeaveReview, ReviewConfirmationModal },
   data () {
     return {
       showMore: false,
-      attributes: [
-        { name: 'language', text: 'Language' },
-        { name: 'pageCount', text: 'Pages' },
-        { name: 'dimensions', text: 'Dimensions' },
-        { name: 'year', text: 'Publication date' },
-        { name: 'publisherName', text: 'Publisher' },
-        { name: 'id', text: 'MySBN' },
-        { name: 'takeOut', text: 'Can be taken out' },
-      ],
       showReviewConfirmation: false,
       userCanReview: false,
     }
@@ -158,18 +135,6 @@ export default {
     },
   },
   methods: {
-    getData (att) {
-      if (att.name === "language") return 'English'
-      if (att.name === "takeOut") return this.data[att.name] ? 'Yes' : 'No'
-      else if (att.name === "publisher") return this.data[att.name]["name"]
-      else if (att.name === "dimensions") {
-        const h = Math.round(this.data.dimensions / 1.61 * 10)
-        const w = Math.round(this.data.dimensions * 10)
-        const g = Math.round(this.data.pageCount / (Math.random()*10 + 10))
-        return h.toString() + ' x ' + w.toString() + ' x ' + g.toString() + 'mm'
-      }
-      else return this.data[att.name]
-    },
     checkUserCanReview() {
       let user_id = document.cookie.split(";")[1].split("=")[1];
       axios
--- frontend/components/book/RelatedEditions.vue
+++ frontend/components/book/RelatedEditions.vue
@@ -1,68 +1,74 @@
 <template>
-  <div v-if="related.length" class="pb-10 mt-6 px-6 lg:px-8">
-    <div class="px-4 hidden lg:block">
-      <VueSlickCarousel :arrows="true" :dots="false" :slidesToShow="3">
-        <div v-for="book in related" :key="book.id">
-          <RelatedEditionCard :data="book" class="px-2"/>
+  <div class="block w-11/12 mt-6 mx-auto bg-white shadow-lg">
+      <div class="block p-8 w-full">
+        <div class="text-xl text-gray-700 tracking-wide font-medium">Related editions</div>
+        
+        <div v-if="related.length" class="pb-10 mt-6 px-6 lg:px-8">
+        <div class="px-4 hidden lg:block">
+          <VueSlickCarousel :arrows="true" :dots="false" :slidesToShow="3">
+            <div v-for="book in related" :key="book.id">
+              <RelatedEditionCard :data="book" class="px-2"/>
+            </div>
+
+            <template #prevArrow="arrowOption">
+              <img :src="require('~/assets/icons/left-arrow.svg')" alt="">
+              <div class="custom-arrow">
+                {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+              </div>
+            </template>
+            <template #nextArrow="arrowOption">
+              <img :src="require('~/assets/icons/right-arrow.svg')" alt="">
+              <div class="custom-arrow">
+                {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+              </div>
+            </template>
+          </VueSlickCarousel>
         </div>
 
-        <template #prevArrow="arrowOption">
-          <img :src="require('~/assets/icons/left-arrow.svg')" alt="">
-          <div class="custom-arrow">
-            {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
-          </div>
-        </template>
-        <template #nextArrow="arrowOption">
-          <img :src="require('~/assets/icons/right-arrow.svg')" alt="">
-          <div class="custom-arrow">
-            {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
-          </div>
-        </template>
-      </VueSlickCarousel>
-    </div>
+        <div class="hidden sm:block lg:hidden px-4">
+          <VueSlickCarousel :arrows="true" :dots="false" :slidesToShow="2">
+            <div  v-for="book in related" :key="book.id">
+              <RelatedEditionCard :data="book" class="px-2"/>
+            </div>
 
-    <div class="hidden sm:block lg:hidden px-4">
-      <VueSlickCarousel :arrows="true" :dots="false" :slidesToShow="2">
-        <div  v-for="book in related" :key="book.id">
-          <RelatedEditionCard :data="book" class="px-2"/>
+            <template #prevArrow="arrowOption">
+              <img :src="require('~/assets/icons/left-arrow.svg')" alt="">
+              <div class="custom-arrow">
+                {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+              </div>
+            </template>
+            <template #nextArrow="arrowOption">
+              <img :src="require('~/assets/icons/right-arrow.svg')" alt="">
+              <div class="custom-arrow">
+                {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+              </div>
+            </template>
+          </VueSlickCarousel>
         </div>
 
-        <template #prevArrow="arrowOption">
-          <img :src="require('~/assets/icons/left-arrow.svg')" alt="">
-          <div class="custom-arrow">
-            {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
-          </div>
-        </template>
-        <template #nextArrow="arrowOption">
-          <img :src="require('~/assets/icons/right-arrow.svg')" alt="">
-          <div class="custom-arrow">
-            {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
-          </div>
-        </template>
-      </VueSlickCarousel>
-    </div>
+        <div class="sm:hidden px-4">
+          <VueSlickCarousel :arrows="true" :dots="false" :slidesToShow="1">
+            <div  v-for="book in related" :key="book.id">
+              <RelatedEditionCard :data="book" class="px-2"/>
+            </div>
 
-    <div class="sm:hidden px-4">
-      <VueSlickCarousel :arrows="true" :dots="false" :slidesToShow="1">
-        <div  v-for="book in related" :key="book.id">
-          <RelatedEditionCard :data="book" class="px-2"/>
+            <template #prevArrow="arrowOption">
+              <img :src="require('~/assets/icons/left-arrow.svg')" alt="">
+              <div class="custom-arrow">
+                {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+              </div>
+            </template>
+            <template #nextArrow="arrowOption">
+              <img :src="require('~/assets/icons/right-arrow.svg')" alt="">
+              <div class="custom-arrow">
+                {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+              </div>
+            </template>
+          </VueSlickCarousel>
         </div>
 
-        <template #prevArrow="arrowOption">
-          <img :src="require('~/assets/icons/left-arrow.svg')" alt="">
-          <div class="custom-arrow">
-            {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
-          </div>
-        </template>
-        <template #nextArrow="arrowOption">
-          <img :src="require('~/assets/icons/right-arrow.svg')" alt="">
-          <div class="custom-arrow">
-            {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
-          </div>
-        </template>
-      </VueSlickCarousel>
+      </div>
     </div>
-
   </div>
 
 </template>
