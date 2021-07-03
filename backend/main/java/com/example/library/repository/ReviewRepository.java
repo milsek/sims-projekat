8e7c24c44bc635c6d28ac72a28783f29bdcc3ad2
Dusan Lazic <dusan.lazic@vega.local>
2021-07-04 01:27:19 +0200

    Prevented users from leaving multiple reviews on the same edition

--- backend/main/java/com/example/library/repository/ReviewRepository.java
+++ backend/main/java/com/example/library/repository/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.example.library.repository;
 import com.example.library.model.Review;
 import com.example.library.model.dto.ReviewDisplayDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +16,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByCheckedFalse();
 
     List<Review> findByEdition_IdAndAllowedTrue(Long editionId);
+
+    @Query(value = "select * from review r1 join reservation r2 on r1.book_reservation_id = r2.id where r1.edition_id=?2 and r2.user_id=?1", nativeQuery = true)
+    Review findByMemberIdAndEditionId(Long userId, Long editionId);
 }
